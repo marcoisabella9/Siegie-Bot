9a7ab002e5c76641c8eb2ee22709e577881f70d9
murc0 <92402819+murc0@users.noreply.github.com>
2024-04-03 22:08:35 -0400

    Basic Functionality
    Will reply with number of members with desired activity

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const {Client, IntentsBitField} = require('discord.js');
 
 const client = new Client ({
@@ -7,20 +8,40 @@ const client = new Client ({
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildPresences,
     ]
 });
 
 client.on('ready', (c) => {
-    console.log(`${c.user.username} is online. 😊`);
+    console.log(`${c.user.username} is online. 😊 Type '!checkSiege' to check the Squad!`);
 });
 
 client.on('messageCreate', (message) => {
-    if (message.author.bot) {
-        return;
-    }
-    
-    if (message.content == 'hello') {
-        message.reply('bruh');
+    if (message.content === '!checkSiege') {
+        const guild = message.guild;
+        const activityToCheck = 'Rainbow Six Siege';  //can change activity to any desired game
+
+        let count = 0;
+        guild.members.cache.forEach(member => {
+            member.presence.activities.forEach(activity => {
+                if (activity.type === 'PLAYING' && activity.name === activityToCheck) {
+                    count++;
+                }
+            });
+        });
+
+        if (count < 1) {
+            message.channel.send(`Nobody is on Siege right now... 😒`);
+        }
+        else if (count < 4) {
+            message.channel.send(`Siege Squad could use some more right now, only ${count} on! 👀`);
+        }
+        else if (count >= 5) {
+            message.channel.send(`Siege Squad is full... 😞 better luck next time.`);
+        }
+        else {
+            message.channel.send(`Act Fast! Only one more spot on Siege! 🏃‍♂️`);
+        }
     }
 })
 
